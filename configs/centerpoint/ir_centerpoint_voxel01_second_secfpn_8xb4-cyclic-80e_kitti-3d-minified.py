e806fd562d6f85175bcf8e693e310ccd41575bbf
chrisis <chrisis58@outlook.com>
2026-07-05 16:55:45 +0800

    fix: update KITTI configs to reference cyclic-80e schedule
    The previous commit renamed config files but did not update the _base_
    reference inside the files from cyclic-20e to cyclic-80e.

--- configs/centerpoint/ir_centerpoint_voxel01_second_secfpn_8xb4-cyclic-80e_kitti-3d-minified.py
+++ configs/centerpoint/ir_centerpoint_voxel01_second_secfpn_8xb4-cyclic-80e_kitti-3d-minified.py
@@ -1,7 +1,7 @@
 _base_ = [
     '../_base_/datasets/kitti-3d-3class.py',
     '../_base_/models/ir_centerpoint_voxel01_second_secfpn_kitti.py',
-    '../_base_/schedules/cyclic-20e.py', '../_base_/default_runtime.py'
+    '../_base_/schedules/cyclic-80e.py', '../_base_/default_runtime.py'
 ]
 
 # If point cloud range is changed, the models should also change their point
